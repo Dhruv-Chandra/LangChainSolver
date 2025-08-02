fad2a65c8b959f3aca6b5a802a736ba35f0a3d85
Dhruv-Chandra <dhruvchandra0401@gmail.com>
2025-08-02 19:23:35 +0530

    minor changes

--- core.py
+++ core.py
@@ -1,7 +1,8 @@
 import os, asyncio
-# from dotenv import load_dotenv
 
-# load_dotenv()
+from dotenv import load_dotenv
+
+
 from langchain.chains.history_aware_retriever import create_history_aware_retriever
 from langchain import hub
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
@@ -9,6 +10,26 @@ from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains.retrieval import create_retrieval_chain
 from langchain_pinecone import PineconeVectorStore
 import streamlit as st
+import datetime, json, base64
+
+
+def save_output(query, vectorstore):
+    date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    res = f"[{date}]\n"
+    res += f"Query: {query}\n"
+
+    retrieved_documents_with_scores = vectorstore.similarity_search_with_score(query)
+
+    for doc, score in retrieved_documents_with_scores:
+        res += f"Document ID: {doc.id}\n"
+        res += f"Source: {doc.metadata["source"]}\n"
+        res += f"Document: {doc.page_content}\n"
+        res += f"Relevance Score: {score}\n"
+        res += "-" * 50 + "\n"
+
+    with open("./LangChainSolver/result/result.txt", "a") as f:
+        f.write(res)
+        f.write("\n")
 
 
 def get_llm_output(query, chat_history):
@@ -19,33 +40,28 @@ def get_llm_output(query, chat_history):
     gemini_embed_model = st.secrets["GEMINI_EMBED_MODEL"]
     pinecone_index = st.secrets["INDEX_NAME"]
     pinecone_api = st.secrets["PINECONE_API_KEY"]
-    os.environ["PINECONE_API_KEY"] = pinecone_api
-    google_application_credentials = st.secrets["GOOGLE_APPLICATION_CREDENTIALS"]
 
     # Local
     # gemini_api = os.getenv("GEMINI_API")
     # gemini_model = os.getenv("GEMINI_MODEL")
-    # gemini_model = os.getenv(["GEMINI_MODEL"])
-    # gemini_embed_model = os.getenv(["GEMINI_EMBED_MODEL"])
-    # pinecone_index = os.getenv(["PINECONE_INDEX"])
-    # pinecone_api = os.getenv(["PINECONE_API"])
-    # google_application_credentials = os.getenv(["GOOGLE_APPLICATION_CREDENTIALS"])
+    # gemini_model = os.getenv("GEMINI_MODEL")
+    # gemini_embed_model = os.getenv("GEMINI_EMBED_MODEL")
+    # pinecone_index = os.getenv("INDEX_NAME")
+    # pinecone_api = os.getenv("PINECONE_API_KEY")
+    
+    os.environ["PINECONE_API_KEY"] = pinecone_api
 
-    # if asyncio.get_event_loop_policy().get_event_loop() is None:
     asyncio.set_event_loop(asyncio.new_event_loop())
     embed = GoogleGenerativeAIEmbeddings(
         model=gemini_embed_model,
-        credentials=google_application_credentials,
+        google_api_key=gemini_api
     )
 
-    # new_vectorstore_faiss = FAISS.load_local("./LangChainSolver/VectorDB", embed, allow_dangerous_deserialization=True)
     new_vectorstore_pinecone = PineconeVectorStore(
         index_name=pinecone_index, embedding=embed
     )
 
-    chat = ChatGoogleGenerativeAI(
-        model=gemini_model, google_api_key=gemini_api
-    )
+    chat = ChatGoogleGenerativeAI(model=gemini_model, google_api_key=gemini_api)
 
     rephrase_prompt = hub.pull("langchain-ai/chat-langchain-rephrase")
     retrieval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
@@ -54,7 +70,7 @@ def get_llm_output(query, chat_history):
 
     history_aware_retriever = create_history_aware_retriever(
         llm=chat,
-        retriever=new_vectorstore_pinecone.as_retriever(),
+        retriever=new_vectorstore_pinecone.as_retriever(search_kwargs={"k": 5}),
         prompt=rephrase_prompt,
     )
 
@@ -63,4 +79,5 @@ def get_llm_output(query, chat_history):
     )
 
     result = qa.invoke(input={"input": query, "chat_history": chat_history})
+    save_output(query, new_vectorstore_pinecone)
     return result
